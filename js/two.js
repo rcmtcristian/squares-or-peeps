b485594f16c7d2a2069e9406f85e8878109ab0cf
rcmtcristian <rcmtcristian@gmail.com>
2022-08-03 15:37:32 -0400

    mouse in process

--- js/two.js
+++ js/two.js
@@ -2,9 +2,39 @@
 let cont = document.querySelector('.hero')
 
 
-
 const two = new Two({
-  type: Tow.Types.svg,
+  type: Two.Types.svg,
   fullscreen: true,
   autostart: true
-}).appendTo(hero)
+}).appendTo(cont);
+
+// two.renderer.domElement.style.background = '#fcb215';
+// two.renderer.domElement.style.cursor = 'none';
+
+const cx = two.width / 2;
+const cy = two.height / 2;
+const delta = new Two.Vector();
+const mouse = new Two.Vector(cx, cy);
+const drag = 0.33;
+const radius = 10;
+
+let cylinder = new Two.Circle(0,0, radius, 32);
+
+let ball = new Two.Path(shape.vertices, true, true);
+ball.position.set(cx, cy);
+ball.noStroke().fill = '#f1f1f1'
+
+for (let i = 0; i < ball.vertices.length; i++) {
+  const v = ball.vertices[i];
+  v.origin = v.clone();
+}
+
+
+two.add( ball);
+
+
+window.addEventListener('mousemove', function (e) {
+  mouse.x = e.clientX;
+  mouse.y = e.clientY;
+
+}, false);
