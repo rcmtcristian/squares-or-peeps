561a7b9049f10e59257ebcc6ad06730adbe6c466
rcmtcristian <rcmtcristian@gmail.com>
2022-08-05 17:16:53 -0400

    so close

--- js/anime.js
+++ js/anime.js
@@ -15,21 +15,6 @@ const playIt = anime({
   delay: anime.stagger(200, { grid: [14, 5], from: 'center' }),
   easing: 'easeInOutQuad',
   delay: anime.stagger(600, { grid: [18, 5], from: 'center' }),
-  // translateX: function (el) {
-  //   return el.getAttribute('data-x');
-  // },
-  // translateY: function (el, i) {
-  //   return 50 + (-50 * i);
-  // },
-  // scale: function (el, i, l) {
-  //   return (l - i) + .25;
-  // },
-  // rotate: function () { return anime.random(-360, 360); },
-  // borderRadius: function () { return ['50%', anime.random(10, 35) + '%']; },
-  // duration: function () { return anime.random(1200, 1800); },
-  // delay: function () { return anime.random(0, 400); },
-
-
 
   loop: true,
   direction: 'alternate',
